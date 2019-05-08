2b919f084186dff3862e117320acf13c2936cfa5
aixoahiryu <aixoahiryu@gmail.com>
2019-05-08 09:18:24 +0700

    Initial commit

---
+++ src/app/resizer/resizer.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+
+@Component({
+	selector: "app-resizer",
+	templateUrl: "./resizer.component.html",
+	styleUrls: ["./resizer.component.css"]
+})
+export class ResizerComponent implements OnInit {
+	ngOnInit() {}
+
+}
